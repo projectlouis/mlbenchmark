3dfe7a3d480f94042c5fb143953a82b899f67e9a
Louis Yu <project.louis@gmail.com>
2017-03-14 20:35:45 -0500

    Add files via upload

---
+++ mnist/conv_lenet5_mxnet.py
@@ -0,0 +1,101 @@
+"""
+Train mnist, see more explanation at http://mxnet.io/tutorials/python/mnist.html
+"""
+import os
+import argparse
+import logging
+logging.basicConfig(level=logging.DEBUG)
+from common import find_mxnet, fit
+from common.util import download_file
+import mxnet as mx
+import numpy as np
+import gzip, struct
+
+def read_data(label, image):
+    """
+    download and read data into numpy
+    """
+    base_url = 'http://yann.lecun.com/exdb/mnist/'
+    with gzip.open(os.path.join('data',label)) as flbl:
+        magic, num = struct.unpack(">II", flbl.read(8))
+        label = np.fromstring(flbl.read(), dtype=np.int8)
+    with gzip.open(os.path.join('data',image), 'rb') as fimg:
+        magic, num, rows, cols = struct.unpack(">IIII", fimg.read(16))
+        image = np.fromstring(fimg.read(), dtype=np.uint8).reshape(len(label), rows, cols)
+    return (label, image)
+
+
+def to4d(img):
+    """
+    reshape to 4D arrays
+    """
+    return img.reshape(img.shape[0], 1, 28, 28).astype(np.float32)/255
+
+def get_mnist_iter(args, kv):
+    """
+    create data iterator with NDArrayIter
+    """
+    (train_lbl, train_img) = read_data(
+            'train-labels-idx1-ubyte.gz', 'train-images-idx3-ubyte.gz')
+    (val_lbl, val_img) = read_data(
+            't10k-labels-idx1-ubyte.gz', 't10k-images-idx3-ubyte.gz')
+    train = mx.io.NDArrayIter(
+        to4d(train_img), train_lbl, args.batch_size, shuffle=True)
+    val = mx.io.NDArrayIter(
+        to4d(val_img), val_lbl, args.batch_size)
+    return (train, val)
+
+if __name__ == '__main__':
+    # parse args
+    parser = argparse.ArgumentParser(description="train mnist",
+                                     formatter_class=argparse.ArgumentDefaultsHelpFormatter)
+    parser.add_argument('--num-classes', type=int, default=10,
+                        help='the number of classes')
+    parser.add_argument('--num-examples', type=int, default=60000,
+                        help='the number of training examples')
+    fit.add_fit_args(parser)
+    parser.set_defaults(
+        # network
+        network        = 'lenet',
+        # train
+        gpus           = None,
+        batch_size      = 64,
+        disp_batches = 100,
+        num_epochs     = 20,
+        lr             = .05,
+        lr_step_epochs = '10',
+    )
+    args = parser.parse_args()
+
+    # load network
+    from importlib import import_module
+    net = import_module('symbol.'+args.network)
+    sym = net.get_symbol(**vars(args))
+
+    # train
+    fit.fit(args, sym, get_mnist_iter)
+	
+def get_symbol(num_classes=10, add_stn=False, **kwargs):
+    data = mx.symbol.Variable('data')
+    if(add_stn):
+        data = mx.sym.SpatialTransformer(data=data, loc=get_loc(data), target_shape = (28,28),
+                                         transform_type="affine", sampler_type="bilinear")
+    # first conv
+    conv1 = mx.symbol.Convolution(data=data, kernel=(5,5), num_filter=20)
+    tanh1 = mx.symbol.Activation(data=conv1, act_type="tanh")
+    pool1 = mx.symbol.Pooling(data=tanh1, pool_type="max",
+                              kernel=(2,2), stride=(2,2))
+    # second conv
+    conv2 = mx.symbol.Convolution(data=pool1, kernel=(5,5), num_filter=50)
+    tanh2 = mx.symbol.Activation(data=conv2, act_type="tanh")
+    pool2 = mx.symbol.Pooling(data=tanh2, pool_type="max",
+                              kernel=(2,2), stride=(2,2))
+    # first fullc
+    flatten = mx.symbol.Flatten(data=pool2)
+    fc1 = mx.symbol.FullyConnected(data=flatten, num_hidden=500)
+    tanh3 = mx.symbol.Activation(data=fc1, act_type="tanh")
+    # second fullc
+    fc2 = mx.symbol.FullyConnected(data=tanh3, num_hidden=num_classes)
+    # loss
+    lenet = mx.symbol.SoftmaxOutput(data=fc2, name='softmax')
+    return lenet
